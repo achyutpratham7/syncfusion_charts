2bf17bfdae76cbd8f758328982c8de4b9792342a
achyutpratham77 <achyutpratham77@gmail.com>
2023-03-06 15:05:03 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late List<GDPData> _chartData;
+  late TooltipBehavior _tooltipBehavior;
+
+  @override
+  void initState() {
+    _chartData = getChartData();
+    _tooltipBehavior = TooltipBehavior(enable: true);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            body: SfCircularChart(
+      title:
+          ChartTitle(text: 'Continent wise GDP - 2021 \n (in billions of USD)'),
+      legend:
+          Legend(isVisible: true, overflowMode: LegendItemOverflowMode.wrap),
+      tooltipBehavior: _tooltipBehavior,
+      series: <CircularSeries>[
+        RadialBarSeries<GDPData, String>(
+          dataSource: _chartData,
+          xValueMapper: (GDPData data, _) => data.continent,
+          yValueMapper: (GDPData data, _) => data.gdp,
+          dataLabelSettings: DataLabelSettings(isVisible: true),
+          enableTooltip: true,
+        )
+      ],
+    )));
+  }
+
+  List<GDPData> getChartData() {
+    final List<GDPData> chartData = [
+      GDPData('Oceania', 16000),
+      GDPData('Africa', 24090),
+      GDPData('S America', 29000),
+      GDPData('Europe', 23050),
+      GDPData('N America', 24880),
+      GDPData('Asia', 34390),
+    ];
+    return chartData;
+  }
+}
+
+class GDPData {
+  GDPData(this.continent, this.gdp);
+  final String continent;
+  final int gdp;
+}
